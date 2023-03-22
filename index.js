7f4750e7b6a3229fbfef84649f561d0e5a62c84e
thelewisndegwa <lewiskanyi77@gmail.com>
2023-03-22 04:49:57 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+
+
+
+const destructivelyAppendCat = ['Milo','Otis','Garfield']
+destructivelyAppendCat.push('Gift')
+
+const destructivelyPrependCat = ['Milo','Otis','Garfield']
+destructivelyPrependCat.unshift('Ginger')
+
+const destructivelyRemoveLastCat = ['Milo','Otis','Garfield']
+destructivelyRemoveLastCat.pop('Garfield')
+
+const appendCat = [...cats, 'Angela']
+
+
+const prependCat = ['Angela',...cats ]
+
+const removeLastCat = cats.slice(-1)
+
+const removeFirstCat = cats.slice(1)
